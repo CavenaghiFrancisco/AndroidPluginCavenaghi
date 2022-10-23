8e7e3e1f7d688f6c4ee53cc7039d070fbfcac890
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2022-10-23 17:59:54 -0300

    Add basic functions

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private ScrollRect scrollRect;
+    [SerializeField] private TMPro.TMP_InputField LogInput;
+
+
+    private void Start()
+    {
+        
+    }
+
+    public void SendLogsButtonPressed()
+    {
+    
+    }
+
+    public void ClearLogsButtonPressed()
+    {
+
+    }
+
+}
